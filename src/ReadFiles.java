b589231097e357c8311663c5f2da8362a68b38b8
sergeyWasiliev <sergeyWasiliev@gmail.com>
2024-11-01 13:05:47 +0300

    Iterator<String> lines = Files.lines(filePath, StandardCharsets.UTF_8).iterator();

--- src/ReadFiles.java
+++ src/ReadFiles.java
@@ -4,20 +4,26 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 public class ReadFiles {
     Cypher cypher = new Cypher();
 
     //Читает файл по строкам через буффер
     public void readfile(Path filePath) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(String.valueOf(filePath), StandardCharsets.UTF_8))) {
-            String lineRead;
-            while ((lineRead = bufferedReader.readLine()) != null) {
-                cypher.encoder(lineRead);
-                //System.out.println(lineRead);
+        try {
+            String line = "";
+            Iterator<String> lines = Files.lines(filePath, StandardCharsets.UTF_8).iterator();
+            while (lines.hasNext()) {
+                line = lines.next();
+                cypher.encoder(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
